5882b266d9ba346df00b698cef53ac264e66aea8
Miyo Bran <miyenbran@gmail.com>
2025-07-31 23:37:52 -0300

    Remove all files related to the JS Game project, including HTML, CSS, JavaScript, and image assets, to clean up the repository and eliminate unused code.

--- 2035-logica-programacion-2-Aula1/app.js
+++
@@ -1,9 +0,0 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = '¡Juego del Número Secreto!';
-
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = 'Adivina el número entre 1 y 10';
-
-function intentoUsuario() {
-    alert('¡Intento realizado!');
-}
